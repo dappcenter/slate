4957d7e92a514df0a02ceb3c709c27de6f92a75d
Trey Griffith <trey.griffith@gmail.com>
2018-07-17 15:32:50 -0700

    update deploy to use now subdomain

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,20 @@
 
 ./generate-md.sh
 
+cd $(dirname "$0")
+
 echo "Building html output..."
 
 bundle exec middleman build --clean || exit 1
 
-echo "Deploying using now.sh"
+echo "Deploying using now.sh..."
+# Because we are deploying to /docs/broker/api, we need that to be our path for zeit - it doesn't strip pathnames
+rm -rf now
+mkdir now
+mkdir now/docs
+mkdir now/docs/broker
+cp -r build now/docs/broker/api
+cp now.json now/now.json
 
-now build
+now now || exit 1
+now alias $(pbpaste) docs-broker-api.kinesis.network
